014166045d1508612c654367bf4aea7d3778f486
Dev-Anky07 <anky007cj@gmail.com>
2023-10-13 16:46:05 +0530

    v1.1 : Optimised suboptimal code

--- query.js
+++ query.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
-
+const fs = require('fs');
 const url = `https://api.etherscan.io/api?module=account&action=tokentx&address=${process.env.ETHEREUM_ADDRESS}&page=1&offset=100&startblock=0&endblock=27025780&sort=asc&apikey=${process.env.ETHERSCAN_API_KEY}`;
 
 fetch(url)
@@ -14,3 +14,15 @@ fetch(url)
     console.log(data);
   })
   .catch(error => console.error('Error:', error));
+
+
+  fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    fs.writeFile('data.json', JSON.stringify(data, null, 2), (err) => {
+      if (err) throw err;
+      console.log('Data written to file');
+    });
+  })
+  .catch(error => console.error('Error:', error));
+
